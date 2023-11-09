cfa3cc29f51c01ec8698f01d304e344884861a77
Manish <anitasharma4440@gmail.com>
2023-11-09 18:47:01 +0530

    new : button rotation

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const theme = document.getElementById("theme");
 const button = document.getElementById("button");
+const btn_circlr = document.getElementById("btn-circle");
 let light = true; 
 
 button.addEventListener("click", () => {
     if(light) {
         theme.href = "css/dark.css";
+        btn_circlr.style.transform = "rotate(90deg)";
         light = !light;
     } else {
         theme.href = "css/light.css";
+        btn_circlr.style.transform = "rotate(0deg)";
         light = !light;
     }
     console.log(light);
